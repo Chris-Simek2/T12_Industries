b6faeaef102c28f29c60d0d1338dcf54981c3d81
Chris-Simek2 <90861341+Chris-Simek2@users.noreply.github.com>
2023-02-05 10:11:18 -0600

    Sensor setup files
    These files set up most of the sensors individually and together

---
+++ Ultrasonic_LED_Speaker.ino
@@ -0,0 +1,63 @@
+// defines pins numbers for ultrasonic
+const int trigPin = 9;
+const int echoPin = 10;
+
+// define pins numbers for LED
+int redPin= 7;
+int greenPin = 6;
+int bluePin = 5;
+
+// define pin numver for speaker
+#define BUZZER 12
+
+// defines variables for ultrasonic 
+long duration;
+int distance;
+
+void setup() {
+  // ultrasonic pin setup
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  // LED pin setup
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  // speaker pin setup
+  pinMode(BUZZER, OUTPUT);
+
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance in cm
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  if (distance <= 35) {
+    setColor(255, 165, 0); // Orange Color
+    tone(BUZZER, 100); //Set the voltage to high and makes a noise
+    delay(500); // helps get rid of flickering
+  } else {
+    noTone(BUZZER); //Sets the voltage to low and makes no noise
+    setColor(0,0,0); // turn LED off
+  }
+}
+
+// funciton to set color of RGB LED
+void setColor(int redValue, int greenValue, int blueValue) {
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
+}
