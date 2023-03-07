4d2d044d3fcd7b7cdeec1a05019cb98fd344c5d5
Chris-Simek2 <90861341+Chris-Simek2@users.noreply.github.com>
2023-03-06 20:09:07 -0600

    Updated BSD
    Fixed the BSD algorithm to no an object moving away returns false and object moving closer returns true.

--- Blind_Spot_Detection/Blind_Spot_Detection.ino
+++ Blind_Spot_Detection/Blind_Spot_Detection.ino
@@ -3,33 +3,33 @@
   File: Smart_Helmet_BSD
   Date created: 03/05/2023
   Author: Christopher Simek
-  Date last revised: 03/05/2023
+  Date last revised: 03/06/2023
   Last revised author: Christopher Simek
 */
 
-/* ALWAYS UPDATE THE HEADER AFTER EDITING THE FILE!!!!*/
+/* ALWAYS UPDATE THE LAST REVISED AUTHOR AFTER EDITING THE FILE!*/
 
-// LED pin
-const int LED_PIN = 13;
+// Both LED and SPEAKER pin numbers
+const int LED_PIN = 13; // LED PIN: Arduino Uno = 13, Adafruit RP2040 = GPIO1
+const int SPEAKER_PIN = 12; // SPEAKER PIN: Arduino Uno = 12, Adafruit RP2040 = GPIO0
 
-const int trigPin1 = 2; // Left Ultrasonic Trig pin: Arduino Uno = 2, Adafruit RP2040 = 2
-const int echoPin1 = 3; // Left Ultrasonic Echo pin: Arduino Uno = 3, Adafruit RP2040 = 3
-const int trigPin2 = 4; // Middle-Left Ultrasonic Trig pin: Arduino Uno = 4, Adafruit RP2040 = 6
-const int echoPin2 = 5; // Middle-Left Ultrasonic Echo pin: Arduino Uno = 5, Adafruit RP2040 = 7
-const int trigPin3 = 6; // Middle Ultrasonic Trig pin: Arduino Uno = 6, Adafruit RP2040 = 8
-const int echoPin3 = 7; // Middle Ultrasonic Echo pin: Arduino Uno = 7, Adafruit RP2040 = 9
-const int trigPin4 = 8; // Middle-Right Ultrasonic Trig pin: Arduino Uno = 8, Adafruit RP2040 = 10
-const int echoPin4 = 9; // Middle-Right Ultrasonic Echo pin: Arduino Uno = 9, Adafruit RP2040 = 11
-const int trigPin5 = 10; // Right Ultrasonic Trig pin: Arduino Uno = 10, Adafruit RP2040 = 12
-const int echoPin5 = 11; // Right Ultrasonic Echo pin: Arduino Uno = 11, Adafruit RP2040 = 13
+// Ultrasonic sensors trig and echo pin numbers
+const int trigPin1 = 2; // Left Ultrasonic Trig pin: Arduino Uno = 2, Adafruit RP2040 = GPIO2
+const int echoPin1 = 3; // Left Ultrasonic Echo pin: Arduino Uno = 3, Adafruit RP2040 = GPIO3
+const int trigPin2 = 4; // Middle-Left Ultrasonic Trig pin: Arduino Uno = 4, Adafruit RP2040 = GPIO6
+const int echoPin2 = 5; // Middle-Left Ultrasonic Echo pin: Arduino Uno = 5, Adafruit RP2040 = GPIO7
+const int trigPin3 = 6; // Middle Ultrasonic Trig pin: Arduino Uno = 6, Adafruit RP2040 = GPIO8
+const int echoPin3 = 7; // Middle Ultrasonic Echo pin: Arduino Uno = 7, Adafruit RP2040 = GPIO9
+const int trigPin4 = 8; // Middle-Right Ultrasonic Trig pin: Arduino Uno = 8, Adafruit RP2040 = GPIO10
+const int echoPin4 = 9; // Middle-Right Ultrasonic Echo pin: Arduino Uno = 9, Adafruit RP2040 = GPIO11
+const int trigPin5 = 10; // Right Ultrasonic Trig pin: Arduino Uno = 10, Adafruit RP2040 = GPIO12
+const int echoPin5 = 11; // Right Ultrasonic Echo pin: Arduino Uno = 11, Adafruit RP2040 = GPIO13
 
-// long int for the duration of the pulse
-// int for the duration to be converted into a distance in ccm
-long duration; 
-int distance_Left, distance_ML, distance_Middle, distance_MR, distance_Right;
+// int distance_Left, distance_ML, distance_Middle, distance_MR, distance_Right;
 
-int distance;
-// Matrix containing ALL the curr and prev distances from the ultrasonic sensors 
+
+// Matrix containing ALL the curr and prev distances from the ultrasonic sensors
+// FOR TESTING PURPOSES 
 int ultra_matrix[10][1] = {{0},{0},{0},{0},{0},{0},{0},{0},{0},{0}};
 
 // Matrix of the current and previous distances from the ultrasonic sensors
@@ -66,19 +66,10 @@ void setup()
   - Function to get the duration of the time from when the trig pin sends
   out a pulse to when the echo pin receives the pulse back.
   - Set the delay to 10ms.
+  - Converting the duration to cm with distance variable.
 */
-long sonarDuration(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  if (distance > 450) {
-    distance = 450;
-  }
-}
-
+long duration; // pulse length from trig to echo
+int distance = duration * 0.034 / 2; // converting duration to cm
 void sonarDistance(int trigPin, int echoPin) {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -92,10 +83,18 @@ void sonarDistance(int trigPin, int echoPin) {
   }
 }
 
+/*
+- Algorithm for when an object is within 250cm with a +5cm difference
+- As an objects currVal and prevVal decrease, the object is getting closer. If so,
+blindSpotDectection return true.
+- As an objects currVal and prevVal increase, the object is moving away. If so,
+blindSpotDectection returns false.
+- An added + or - 5 cm is added to account for accuracy of the ultrasonic sensors.
+*/
 bool blindSpotDetection(int currVal, int prevVal) {
-  while (currVal  <= 255) {
+  while (currVal  <= 250 + 5) {
     if (currVal - prevVal <= 5 || currVal - prevVal >= -5) {
-      if (currVal - prevVal < -5) {
+      if (currVal - prevVal > 5) {
         return false;
         } else {
         return true;
@@ -105,78 +104,69 @@ bool blindSpotDetection(int currVal, int prevVal) {
   return false;
 }
 
+/* 
+Turns on the LED and Speaker notification system when the blindSpotDectection algorithm 
+detects an object moving within 250cm of the rider.
+*/
+void BSD_notification() {
+  if (blindSpotDetection(MR_Matrix[0][0], MR_Matrix[1][0]) == true) {
+    digitalWrite(LED_PIN, HIGH);
+    tone(SPEAKER_PIN, 100);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+    noTone(SPEAKER_PIN);
+  }
+}
+
 
 void loop() {
   /* 
-    - Call the sonarDuration function to have the ultrasonic sensors send a pulse
-    and return the duration.
-    - Calculate the distance is cm using the equation: distance = duration * 0.034 / 2 
+    - Call the sonarDistance function to have the ultrasonic sensors send a pulse
+    and return the distance.
+    - Store the current and previous distances in the associated matrix.
   */
-/*
   sonarDistance(trigPin1, echoPin1);
-  Serial.print(distance);
+  left_Matrix[1][0] = left_Matrix[0][0];
+  delay(250); 
+  left_Matrix[0][0] = distance;
 
   sonarDistance(trigPin2, echoPin2);
-  Serial.print("-");
-  Serial.print(distance);
+  ML_Matrix[1][0] = ML_Matrix[0][0];
+  delay(250); 
+  ML_Matrix[0][0] = distance;
 
   sonarDistance(trigPin3, echoPin3);
-  Serial.print("-");
-  Serial.print(distance);
-*/
+  middle_Matrix[1][0] = middle_Matrix[0][0];
+  delay(250); 
+  middle_Matrix[0][0] = distance;
   
   sonarDistance(trigPin4, echoPin4);
-
-  for (int i = 9; i > 0; i--) {
-    ultra_matrix[i][0] = ultra_matrix[i-1][0];
-  }
-  ultra_matrix[0][0] = distance;
-
   MR_Matrix[1][0] = MR_Matrix[0][0];
+  delay(250); 
   MR_Matrix[0][0] = distance;
-  if (blindSpotDetection(MR_Matrix[0][0], MR_Matrix[1][0]) == true) {
-    digitalWrite(LED_PIN, HIGH);
-  } else {
-    digitalWrite(LED_PIN, LOW);
-  }
-  
-  Serial.print(MR_Matrix[0][0]);
-  Serial.print("-"); 
-  Serial.println(MR_Matrix[1][0]);
 
-  // sonarDistance(trigPin5, echoPin5);
-  // Serial.print("-");
-  // Serial.println(distance);
- 
-  // sonarDuration(trigPin1, echoPin1);
-  // distance_Left = (duration*0.034)/2;
-  // Serial.print(distance_Left);
-  // Serial.print(distance);
+  sonarDistance(trigPin5, echoPin5);
+  right_Matrix[1][0] = right_Matrix[0][0];
+  delay(250); 
+  right_Matrix[0][0] = distance;
 
-  // sonarDuration(trigPin2, echoPin2);
-  // distance_ML = (duration*0.034)/2;
-  // Serial.print(" - ");
-  // Serial.print(distance_ML);
 
-  // sonarDuration(trigPin3, echoPin3);
-  // distance_Middle = (duration*0.034)/2;
-  // Serial.print(" - ");
-  // Serial.print(distance_Middle);
+  /*
+  - Blind spot dectection algorithm is called for detection.
+  */
+  blindSpotDetection(left_Matrix[0][0], left_Matrix[1][0]);
+  BSD_notification();
 
-  // sonarDuration(trigPin4, echoPin4);
-  // distance_MR = (duration*0.034)/2;
-  // Serial.print(" - ");
-  // Serial.print(distance_MR);
+  blindSpotDetection(ML_Matrix[0][0], ML_Matrix[1][0]);
+  BSD_notification();
 
-  // sonarDuration(trigPin5, echoPin5);
-  // distance_Right = (duration*0.034)/2;
-  // Serial.print(" - ");
-  // Serial.println(distance_Right);
+  blindSpotDetection(middle_Matrix[0][0], middle_Matrix[1][0]);
+  BSD_notification();
 
-  // left_Matrix[1][0] = left_Matrix[0][0];
-  // left_Matrix[0][0] = distance_Left;
+  blindSpotDetection(MR_Matrix[0][0], MR_Matrix[1][0]);
+  BSD_notification();
 
-  // Serial.println(left_Matrix[0][0]);
-  // Serial.println(left_Matrix[1][0]);
+  blindSpotDetection(right_Matrix[0][0], right_Matrix[1][0]);
+  BSD_notification();
 
 }
