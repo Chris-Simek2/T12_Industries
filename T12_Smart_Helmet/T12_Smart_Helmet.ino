215e2ece3f22c5727b7d3bea92fd800ee33b8399
Chris-Simek2 <90861341+Chris-Simek2@users.noreply.github.com>
2023-04-20 02:49:51 -0500

    Update T12_Smart_Helmet.ino
    idk wth is going on with the power

--- T12_Smart_Helmet/T12_Smart_Helmet.ino
+++ T12_Smart_Helmet/T12_Smart_Helmet.ino
@@ -16,8 +16,10 @@ uint16_t measurement_delay_us = 65535; // Delay between measurements for testing
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // Both LED and SPEAKER pin numbers
-const int LED_PIN = 1; // LED PIN: Arduino Uno = 13, Adafruit RP2040 = GPIO1
-const int SPEAKER_PIN = 0; // SPEAKER PIN: Arduino Uno = 12, Adafruit RP2040 = GPIO0
+const int right_LED_PIN = 18; // LED PIN: Arduino Uno = 13, Adafruit RP2040 = GPIO1
+const int left_SPEAKER_PIN = 0; // SPEAKER PIN: Arduino Uno = 12, Adafruit RP2040 = GPIO0
+const int left_LED_PIN = 1;
+const int right_SPEAKER_PIN = 20;
 
 // Ultrasonic sensors trig and echo pin numbers
 const int trigPin1 = 2; // Left Ultrasonic Trig pin: Arduino Uno = 2, Adafruit RP2040 = GPIO2
@@ -57,9 +59,11 @@ void setup(void) {
   pinMode(trigPin5, OUTPUT);
   pinMode(echoPin5, INPUT);
 
-  // LED and SPEAKER
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(SPEAKER_PIN, OUTPUT);
+  // LEDs and SPEAKERs
+  pinMode(left_LED_PIN, OUTPUT);
+  pinMode(left_SPEAKER_PIN, OUTPUT);
+  pinMode(right_LED_PIN, OUTPUT);
+  pinMode(right_SPEAKER_PIN, OUTPUT);
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
   Serial.begin(115200);
@@ -183,7 +187,7 @@ blindSpotDectection returns false.
 - An added + or - 5 cm is added to account for accuracy of the ultrasonic sensors.
 */
 bool blindSpotDetection(int currVal, int prevVal1, int prevVal2, int prevVal3) {
-  while (currVal  <= 240) {
+  while (currVal  <= 50) {
     if (currVal - prevVal1 > 5 && prevVal2 - prevVal3 > 5) {
       return false;
     } else {
@@ -197,13 +201,41 @@ bool blindSpotDetection(int currVal, int prevVal1, int prevVal2, int prevVal3) {
 Turns on the LED and Speaker notification system when the blindSpotDectection algorithm 
 detects an object moving within 250cm of the rider.
 */
-void BSD_notification(int currVal, int prevVal1, int prevVal2, int prevVal3) {
+void center_BSD_notification(int currVal, int prevVal1, int prevVal2, int prevVal3) {
   if (blindSpotDetection(currVal, prevVal1, prevVal2, prevVal3) == true) {
-    digitalWrite(LED_PIN, HIGH);
-    tone(SPEAKER_PIN, 100);
+    digitalWrite(left_LED_PIN, HIGH);
+    digitalWrite(right_LED_PIN, HIGH);
+    tone(left_SPEAKER_PIN, 100);
+    tone(right_SPEAKER_PIN, 100);
   } else {
-    digitalWrite(LED_PIN, LOW);
-    noTone(SPEAKER_PIN);
+    digitalWrite(left_LED_PIN, LOW);
+    digitalWrite(right_LED_PIN, LOW);
+    noTone(left_SPEAKER_PIN);
+    noTone(right_SPEAKER_PIN);
+  }
+}
+
+void right_BSD_notification(int currVal, int prevVal1, int prevVal2, int prevVal3) {
+  if (blindSpotDetection(currVal, prevVal1, prevVal2, prevVal3) == true) {
+    digitalWrite(right_LED_PIN, HIGH);
+    tone(right_SPEAKER_PIN, 100);
+  } else {
+    // digitalWrite(left_LED_PIN, LOW);
+    digitalWrite(right_LED_PIN, LOW);
+    // noTone(left_SPEAKER_PIN);
+    noTone(right_SPEAKER_PIN);
+  }
+}
+
+void left_BSD_notification(int currVal, int prevVal1, int prevVal2, int prevVal3) {
+  if (blindSpotDetection(currVal, prevVal1, prevVal2, prevVal3) == true) {
+    digitalWrite(left_LED_PIN, HIGH);
+    tone(left_SPEAKER_PIN, 100);
+  } else {
+    digitalWrite(left_LED_PIN, LOW);
+    // digitalWrite(right_LED_PIN, LOW);
+    noTone(left_SPEAKER_PIN);
+    // noTone(right_SPEAKER_PIN);
   }
 }
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -285,68 +317,81 @@ Serial.print(gyro.gyro.z*180/PI);
     }
 
  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ // Adding left ultrasonic sensor values into matrix
   sonarDistance(trigPin1, echoPin1);
-  left_Matrix[1][0] = left_Matrix[0][0];
-  delay(10); 
+  for (int i = 3; i > 0; i--){
+    left_Matrix[i][0] = left_Matrix[i-1][0];
+  }
+  delay(10);
   left_Matrix[0][0] = distance;
+  blindSpotDetection(left_Matrix[0][0], left_Matrix[1][0], left_Matrix[2][0], left_Matrix[3][0]);
+  left_BSD_notification(left_Matrix[0][0], left_Matrix[1][0], left_Matrix[2][0], left_Matrix[3][0]);
 
+  // Adding ML ultrasonic sensor values into matrix
   sonarDistance(trigPin2, echoPin2);
-  ML_Matrix[1][0] = ML_Matrix[0][0];
-  delay(10); 
+  for (int i = 3; i > 0; i--){
+    ML_Matrix[i][0] = ML_Matrix[i-1][0];
+  }
+  delay(10);
   ML_Matrix[0][0] = distance;
+  // blindSpotDetection(ML_Matrix[0][0], ML_Matrix[1][0], ML_Matrix[2][0], ML_Matrix[3][0]);
+  // left_BSD_notification(ML_Matrix[0][0], ML_Matrix[1][0], ML_Matrix[2][0], ML_Matrix[3][0]);
 
+  // Adding middle ultrasonic sensor values into matrix
   sonarDistance(trigPin3, echoPin3);
   for (int i = 3; i > 0; i--){
     middle_Matrix[i][0] = middle_Matrix[i-1][0];
   }
   delay(10);
   middle_Matrix[0][0] = distance;
-
-  // middle_Matrix[1][0] = middle_Matrix[0][0];
-  // delay(10); 
-  // middle_Matrix[0][0] = distance;
+  blindSpotDetection(middle_Matrix[0][0], middle_Matrix[1][0], middle_Matrix[2][0], middle_Matrix[3][0]);
+  center_BSD_notification(middle_Matrix[0][0], middle_Matrix[1][0], middle_Matrix[2][0], middle_Matrix[3][0]);
   
+  // Adding MR ultrasonic sensor values into matrix
   sonarDistance(trigPin4, echoPin4);
   for (int i = 3; i > 0; i--){
-    matrix4[i][0] = matrix4[i-1][0];
+    MR_Matrix[i][0] = MR_Matrix[i-1][0];
   }
   delay(10);
-  matrix4[0][0] = distance;
-
-  // MR_Matrix[1][0] = MR_Matrix[0][0];
-  // delay(50); 
-  // MR_Matrix[0][0] = distance;
+  MR_Matrix[0][0] = distance;
+  // blindSpotDetection(MR_Matrix[0][0], MR_Matrix[1][0], MR_Matrix[2][0], MR_Matrix[3][0]);
+  // right_BSD_notification(MR_Matrix[0][0], MR_Matrix[1][0], MR_Matrix[2][0], MR_Matrix[3][0]);
 
   sonarDistance(trigPin5, echoPin5);
-  right_Matrix[1][0] = right_Matrix[0][0];
-  delay(10); 
+  for (int i = 3; i > 0; i--){
+    right_Matrix[i][0] = right_Matrix[i-1][0];
+  }
+  delay(10);
   right_Matrix[0][0] = distance;
+  blindSpotDetection(right_Matrix[0][0], right_Matrix[1][0], right_Matrix[2][0], right_Matrix[3][0]);
+  right_BSD_notification(right_Matrix[0][0], right_Matrix[1][0], right_Matrix[2][0], right_Matrix[3][0]);
 
   /*
   - Blind spot dectection algorithm is called for detection.
   */
   // blindSpotDetection(left_Matrix[0][0], left_Matrix[1][0], left_Matrix[2][0], left_Matrix[3][0]);
-  // BSD_notification(left_Matrix[0][0], left_Matrix[1][0], left_Matrix[2][0], left_Matrix[3][0]);
+  // left_BSD_notification(left_Matrix[0][0], left_Matrix[1][0], left_Matrix[2][0], left_Matrix[3][0]);
 
   // blindSpotDetection(ML_Matrix[0][0], ML_Matrix[1][0], ML_Matrix[2][0], ML_Matrix[3][0]);
-  // BSD_notification(ML_Matrix[0][0], ML_Matrix[1][0], ML_Matrix[2][0], ML_Matrix[3][0]);
+  // left_BSD_notification(ML_Matrix[0][0], ML_Matrix[1][0], ML_Matrix[2][0], ML_Matrix[3][0]);
 
-  blindSpotDetection(middle_Matrix[0][0], middle_Matrix[1][0], middle_Matrix[2][0], middle_Matrix[3][0]);
-  BSD_notification(middle_Matrix[0][0], middle_Matrix[1][0], middle_Matrix[2][0], middle_Matrix[3][0]);
+  // blindSpotDetection(middle_Matrix[0][0], middle_Matrix[1][0], middle_Matrix[2][0], middle_Matrix[3][0]);
+  // center_BSD_notification(middle_Matrix[0][0], middle_Matrix[1][0], middle_Matrix[2][0], middle_Matrix[3][0]);
 
   // blindSpotDetection(MR_Matrix[0][0], MR_Matrix[1][0], MR_Matrix[2][0], MR_Matrix[3][0]);
-  // BSD_notification(MR_Matrix[0][0], MR_Matrix[1][0], MR_Matrix[2][0], MR_Matrix[3][0]);
+  // right_BSD_notification(MR_Matrix[0][0], MR_Matrix[1][0], MR_Matrix[2][0], MR_Matrix[3][0]);
 
   // blindSpotDetection(right_Matrix[0][0], right_Matrix[1][0], right_Matrix[2][0], right_Matrix[3][0]);
-  // BSD_notification(right_Matrix[0][0], right_Matrix[1][0], right_Matrix[2][0], right_Matrix[3][0]);
+  // right_BSD_notification(right_Matrix[0][0], right_Matrix[1][0], right_Matrix[2][0], right_Matrix[3][0]);
 
 
   // PRINTING THE MATRICES FOR TESTING PURPOSES
-  for (int i = 0; i < 3; i++) {
-    Serial.print(middle_Matrix[i][0]);
-    Serial.print("-");
-  }
-  Serial.println(middle_Matrix[3][0]);
+  // for (int i = 0; i < 3; i++) {
+  //   Serial.print(middle_Matrix[i][0]);
+  //   Serial.print("-");
+  // }
+  // Serial.println(middle_Matrix[3][0]);
   ////////////////////////////////////////////////////////////////////////////////////////////////////    
 
   }
